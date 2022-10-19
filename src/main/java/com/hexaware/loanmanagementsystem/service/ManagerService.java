12f71013f5a0f3d9b5cba1632fed7d1a44404bab
Anushka0310 <76205374+Anushka0310@users.noreply.github.com>
2022-10-19 11:04:18 +0530

    Update ManagerService.java

--- src/main/java/com/hexaware/loanmanagementsystem/service/ManagerService.java
+++ src/main/java/com/hexaware/loanmanagementsystem/service/ManagerService.java
@@ -1,13 +1,19 @@
 package com.hexaware.loanmanagementsystem.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.hexaware.loanmanagementsystem.entity.Manager;
+import com.hexaware.loanmanagementsystem.exception.ResourceNotFound;
+
 
 public interface ManagerService {
-	public List<Manager> findAll();
-	public Manager findById(Long manager_id);
-	public void deleteById(Long manager_id);
+	
+	public List<Manager> findAllManager();
+	public Optional<Manager> getManagerById(Long manager_id)throws ResourceNotFound;
+	public void deleteById(Long manager_id)throws ResourceNotFound;
 	public Manager save(Manager manager);
-	public int update(Manager manager,Long manager_id);
+	public int update(Manager manager,Long manager_id)throws ResourceNotFound;
 
+	
 }

efe17b127de618e110ef00a3d66c2be26d45d0b2
Sahista Parween <56756738+sahistaparween@users.noreply.github.com>
2022-10-19 12:09:46 +0530

    Update CustomerRepository.java

--- src/main/java/com/hexaware/loanmanagementsystem/repository/CustomerRepository.java
+++ src/main/java/com/hexaware/loanmanagementsystem/repository/CustomerRepository.java
@@ -1,5 +1,16 @@
 package com.hexaware.loanmanagementsystem.repository;
 
-public interface CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
+
+import com.hexaware.loanmanagementsystem.entity.Customer;
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+
+	
+	
+
+		
 }

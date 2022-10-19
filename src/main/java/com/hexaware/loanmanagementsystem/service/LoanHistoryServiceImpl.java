2716bb7f2f3cef54f63b66c12fc3d98ac5d299ab
Akshithapailla <114604531+Akshithapailla@users.noreply.github.com>
2022-10-19 12:22:52 +0530

    Update LoanHistoryServiceImpl.java

--- src/main/java/com/hexaware/loanmanagementsystem/service/LoanHistoryServiceImpl.java
+++ src/main/java/com/hexaware/loanmanagementsystem/service/LoanHistoryServiceImpl.java
@@ -1,43 +1,46 @@
 package com.hexaware.loanmanagementsystem.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hexaware.loanmanagementsystem.entity.LoanDetails;
 import com.hexaware.loanmanagementsystem.entity.LoanHistory;
 import com.hexaware.loanmanagementsystem.repository.LoanHistoryRepository;
 
-
+import com.hexaware.loanmanagementsystem.exception.ResourceNotFoundException;
 @Service
 public abstract class LoanHistoryServiceImpl implements LoanHistoryService{
+	
+	@Autowired
+	LoanHistoryRepository loanhistoryRepo;
 
-@Autowired
-LoanHistoryRepository loanhistoryRepo;
+	@Override
+	public List<LoanHistory> findAllloan_id() {
+		return loanhistoryRepo.findAll();
+	}
 
-@Override
-public List<LoanHistory> findAll() {
-return loanhistoryRepo.findAll();
-}
+	@Override
 
-@Override
-public LoanHistory findById(Long loan_id) {
-return loanhistoryRepo.findById(loan_id).get();
-}
+	public  Optional<LoanHistory>getLoanById(Long loan_id) throws ResourceNotFoundException {
+		return null;
+	}
 
-@Override
-public void deleteById(Long loan_id) {
-loanhistoryRepo.deleteById(loan_id);
-}
+	@Override
+	public void deleteById(Long loan_id) throws ResourceNotFoundException{
+		loanhistoryRepo.deleteById(loan_id);
+	}
 
-@Override
-public LoanHistory save(LoanHistory loanhistory) {
-return loanhistoryRepo.save(loanhistory);
-}
+	@Override
+	public LoanHistory save(LoanHistory loanhistory) {
+		return loanhistoryRepo.save(loanhistory);
+	}
 
-@Override
-public int update(LoanHistory loanhistory, Long loan_id) {
-return 0;
-}
+	@Override
+	public int  LoanHistoryupdate(LoanHistory loanhistory, Long loan_id)throws ResourceNotFoundException {
+		return 0;
+	}
 
 }

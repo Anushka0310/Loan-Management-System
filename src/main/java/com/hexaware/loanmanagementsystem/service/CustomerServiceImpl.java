7a9fa405fae3bca738ad534522e5bf59ded04fd2
Sahista Parween <56756738+sahistaparween@users.noreply.github.com>
2022-10-19 12:13:08 +0530

    Update CustomerServiceImpl.java

--- src/main/java/com/hexaware/loanmanagementsystem/service/CustomerServiceImpl.java
+++ src/main/java/com/hexaware/loanmanagementsystem/service/CustomerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.hexaware.loanmanagementsystem.service;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,8 +8,10 @@ import org.springframework.stereotype.Service;
 import com.hexaware.loanmanagementsystem.entity.Customer;
 import com.hexaware.loanmanagementsystem.repository.CustomerRepository;
 
+import com.hexaware.loanmanagementsystem.exception.ResourceNotFoundException;
 
 
+@Service
 public class CustomerServiceImpl implements CustomerService{
 	@Autowired
 	CustomerRepository customerRepo;
@@ -19,12 +22,12 @@ public class CustomerServiceImpl implements CustomerService{
 	}
 
 	@Override
-	public Customer findById_customer(Long customer_id) {
-		return customerRepo.findById(customer_id).get();
+	public Optional<Customer> getCustomerById_customer(Long customer_id) throws ResourceNotFoundException{
+		return customerRepo.findById(customer_id);
 	}
 
 	@Override
-	public void deleteById_customer(Long customer_id) {
+	public void deleteById_customer(Long customer_id)throws ResourceNotFoundException {
 		customerRepo.deleteById(customer_id);
 	}
 
@@ -34,8 +37,12 @@ public class CustomerServiceImpl implements CustomerService{
 	}
 
 	@Override
-	public int update_customer(Customer customer, Long customer_id) {
-		return 0;
+	public Customer update_customerById(Customer newcustomer, Long customer_id)throws ResourceNotFoundException{
+		Customer oldCustomerdetails = getCustomerById_customer(customer_id)
+				.orElseThrow(() -> new ResourceNotFoundException("customer with id" + customer_id + " not found"));
+		oldCustomerdetails.setCustomer_name(newcustomer.getCustomer_name());
+		return customerRepo.save(oldCustomerdetails);
 	}
-
 }
+
+	

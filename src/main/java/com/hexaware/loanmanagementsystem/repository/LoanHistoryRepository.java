290be2130c546e0140e0cc8c59a57f0593bd3fb9
Akshithapailla <114604531+Akshithapailla@users.noreply.github.com>
2022-10-19 10:26:56 +0530

    Update LoanHistoryRepository.java

--- src/main/java/com/hexaware/loanmanagementsystem/repository/LoanHistoryRepository.java
+++ src/main/java/com/hexaware/loanmanagementsystem/repository/LoanHistoryRepository.java
@@ -1,5 +1,12 @@
 package com.hexaware.loanmanagementsystem.repository;
 
-public interface LoanHistoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+
+import com.hexaware.loanmanagementsystem.entity.LoanHistory;
+@Repository
+public interface LoanHistoryRepository extends JpaRepository<LoanHistory,Long> {
 
 }

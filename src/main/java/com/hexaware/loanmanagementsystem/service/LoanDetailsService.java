746002fc97e456924dbd12ab4da37ed02eeb7700
Akshithapailla <114604531+Akshithapailla@users.noreply.github.com>
2022-10-19 10:27:37 +0530

    Update LoanDetailsService.java

--- src/main/java/com/hexaware/loanmanagementsystem/service/LoanDetailsService.java
+++ src/main/java/com/hexaware/loanmanagementsystem/service/LoanDetailsService.java
@@ -1,5 +1,13 @@
 package com.hexaware.loanmanagementsystem.service;
+import java.util.List;
+import com.hexaware.loanmanagementsystem.entity.LoanDetails;
 
 public interface LoanDetailsService {
 
+
+public List<LoanDetails> findAll();
+public LoanDetails findById(Long movieId);
+public void deleteById(Long movieId);
+public LoanDetails save(LoanDetails loandetails);
+public int update(LoanDetails loandetaials,Long loan_id);
 }

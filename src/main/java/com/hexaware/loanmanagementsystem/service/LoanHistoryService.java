64783e9a9875598b62cbf071f5a472220c38b6ad
Akshithapailla <114604531+Akshithapailla@users.noreply.github.com>
2022-10-19 11:37:55 +0530

    Update LoanHistoryService.java

--- src/main/java/com/hexaware/loanmanagementsystem/service/LoanHistoryService.java
+++ src/main/java/com/hexaware/loanmanagementsystem/service/LoanHistoryService.java
@@ -1,14 +1,18 @@
 package com.hexaware.loanmanagementsystem.service;
 import java.util.List;
+import java.util.Optional;
+
 
 import com.hexaware.loanmanagementsystem.entity.LoanHistory;
+import com.hexaware.loanmanagementsystem.exception.ResourceNotFoundException;
 
 
 public interface LoanHistoryService {
 
-    public List<LoanHistory> findAll();
-public LoanHistory findById(Long loan_id);
-public void deleteById(Long loan_id);
-public LoanHistory save(LoanHistory loan_id);
-public int update(LoanHistory loanhistory ,Long Loan_id);
+	public List<LoanHistory> findAllloan_id();
+	public Optional<LoanHistory>getLoanById(Long loan_id)throws ResourceNotFoundException;
+
+	public void deleteById(Long loan_id)throws ResourceNotFoundException;;
+	public LoanHistory save(LoanHistory loanhistory);
+	public int update(LoanHistory loanhistory,Long loan_id)throws ResourceNotFoundException;;
 }

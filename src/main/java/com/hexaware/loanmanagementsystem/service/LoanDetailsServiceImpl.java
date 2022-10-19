00b03f172be2214da7afdd18f4935c152a4ffa7d
Akshithapailla <114604531+Akshithapailla@users.noreply.github.com>
2022-10-19 12:21:37 +0530

    Update LoanDetailsServiceImpl.java

--- src/main/java/com/hexaware/loanmanagementsystem/service/LoanDetailsServiceImpl.java
+++ src/main/java/com/hexaware/loanmanagementsystem/service/LoanDetailsServiceImpl.java
@@ -1,43 +1,47 @@
 package com.hexaware.loanmanagementsystem.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hexaware.loanmanagementsystem.entity.LoanDetails;
 import com.hexaware.loanmanagementsystem.repository.LoanDetailsRepository;
+import com.hexaware.loanmanagementsystem.exception.ResourceNotFoundException;
 
 
 @Service
 public abstract class LoanDetailsServiceImpl implements LoanDetailsService{
+	
+	@Autowired
+	LoanDetailsRepository loandetailsRepo;
 
-@Autowired
-LoanDetailsRepository loandetailsRepo;
+	@Override
+	public List<LoanDetails> findAllloan_id() {
+		return loandetailsRepo.findAll();
+	}
 
-@Override
-public List<LoanDetails> findAll() {
-return loandetailsRepo.findAll();
-}
+	@Override
+	public  Optional<LoanDetails>getLoanById(Long loan_id) throws ResourceNotFoundException
+	{
+		return null;
+	}
 
-@Override
-public LoanDetails findById(Long loan_id) {
-return loandetailsRepo.findById(loan_id).get();
-}
+	@Override
+	public void deleteById(Long loan_id)throws ResourceNotFoundException {
+		loandetailsRepo.deleteById(loan_id);
+	}
 
-@Override
-public void deleteById(Long loan_id) {
-loandetailsRepo.deleteById(loan_id);
-}
+	@Override
+	public LoanDetails save(LoanDetails loandetails) {
+		return loandetailsRepo.save(loandetails);
+	}
 
-@Override
-public LoanDetails save(LoanDetails loandetails) {
-return loandetailsRepo.save(loandetails);
-}
-
-@Override
-public int update(LoanDetails loandetails, Long loan_id) {
-return 0;
-}
+	@Override
+	public int LoanDetailsupdate(LoanDetails loandetails, Long loan_id) throws ResourceNotFoundException
+	{
+		return 0;
+	}
 
 }

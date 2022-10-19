d9e55510a96fc517335674d2516eec2812087229
Sahista Parween <56756738+sahistaparween@users.noreply.github.com>
2022-10-19 12:10:27 +0530

    Update CustomerService.java

--- src/main/java/com/hexaware/loanmanagementsystem/service/CustomerService.java
+++ src/main/java/com/hexaware/loanmanagementsystem/service/CustomerService.java
@@ -1,16 +1,20 @@
 package com.hexaware.loanmanagementsystem.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.hexaware.loanmanagementsystem.entity.Customer;
+import com.hexaware.loanmanagementsystem.exception.ResourceNotFoundException;
 
 
 public interface CustomerService {
+	
 	public List<Customer> findAllCustomers();
-	public Customer findById_customer(Long customer_id);
-	public void deleteById_customer(Long customer_id);
+	public Optional <Customer> getCustomerById_customer(Long customer_id)throws ResourceNotFoundException;
+	public void deleteById_customer(Long customer_id)throws ResourceNotFoundException;
 	public Customer save_customer(Customer customer_id);
-	public int update_customer(Customer customer,Long customer_id);
+	public Customer update_customerById(Customer customer,Long customer_id)throws ResourceNotFoundException;
+	
 
 }
 

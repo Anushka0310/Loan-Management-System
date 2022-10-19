92e69fbb5acae7dd0a043c43a1c36f02ef25b625
Akshithapailla <114604531+Akshithapailla@users.noreply.github.com>
2022-10-19 12:07:12 +0530

    Update LoanHistoryController.java

--- src/main/java/com/hexaware/loanmanagementsystem/controller/LoanHistoryController.java
+++ src/main/java/com/hexaware/loanmanagementsystem/controller/LoanHistoryController.java
@@ -1,6 +1,7 @@
 package com.hexaware.loanmanagementsystem.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hexaware.loanmanagementsystem.entity.LoanHistory;
-
+import com.hexaware.loanmanagementsystem.exception.ResourceNotFoundException;
 import com.hexaware.loanmanagementsystem.service.LoanHistoryService;
 
 
@@ -21,33 +22,35 @@ import com.hexaware.loanmanagementsystem.service.LoanHistoryService;
 @RestController
 @RequestMapping("/api")
 public class LoanHistoryController {
-
-@Autowired
-LoanHistoryService LoanHistoryService;
-
-@GetMapping("/getallloanhistory")
-public List<LoanHistory> getLoanHistory(){
-return LoanHistoryService.findAll();
-}
-
-@PostMapping("/addloanhistory")
-public String saveMovie(@RequestBody LoanHistory loanhistory) {
-return LoanHistoryService.save(loanhistory) + "added successfully";
-}
-
-@DeleteMapping("/deleteloanhistory/{loan_id}")
-public void deleteLoanHistory(@PathVariable("loan_id") Long loan_id) {
-LoanHistoryService.deleteById(loan_id) ;
-}
-
-@GetMapping("/getloanhistory/{loan_id}")
-public LoanHistory getLoanHistory(@PathVariable("loan_id") Long loan_id) {
-return LoanHistoryService.findById(loan_id);
-}
-
-@PutMapping("/updateloanhistory(loan_id}")
-public String Update(@RequestBody LoanHistory loanhistory,@PathVariable("loan_id") Long loan_id) {
-return LoanHistoryService.update(loanhistory, loan_id) + " updated successfully";
-
+	
+	@Autowired
+	LoanHistoryService LoanHistoryService;
+	
+	@GetMapping("/getallloanhistory")
+	public List<LoanHistory> getLoanHistory(){
+		return LoanHistoryService.findAllloan_id();
+	}
+	
+	@PostMapping("/addloanhistory")
+	public String saveMovie(@RequestBody LoanHistory loanhistory) {
+		return LoanHistoryService.save(loanhistory) + "added successfully";
+	}
+	
+	@DeleteMapping("/deleteloanhistory/{loan_id}")
+	public void deleteLoanHistory(@PathVariable("loan_id") Long loan_id) throws ResourceNotFoundException {
+		 LoanHistoryService.deleteById(loan_id) ;
+	}
+	
+	@GetMapping("/getloanhistory/{loan_id}")
+	public Optional<LoanHistory> getLoanHistory(@PathVariable("loan_id") Long loan_id) throws ResourceNotFoundException {
+		return LoanHistoryService.getLoanById(loan_id);
+	}
+	
+	@PutMapping("/updateloanhistory(loan_id}")
+	public String Update(@RequestBody LoanHistory loanhistory,@PathVariable("loan_id") Long loan_id) throws ResourceNotFoundException {
+		return LoanHistoryService.update(loanhistory, loan_id) + " updated successfully";
+		
+	}
+	
 }
 
